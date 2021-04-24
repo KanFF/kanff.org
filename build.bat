73a4a02544f483b3aa3b8bb58766a1e92a4cc56c
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-04-24 15:27:05 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,5 +4,7 @@ cd ..
 curl localhost:8088 -o dist/index.html
 mkdir dist\imgs
 copy imgs dist\imgs
+copy favicon.ico dist\favicon.ico
+copy robots.txt dist\robots.txt
 tree /F dist
 start firefox dist/index.html
