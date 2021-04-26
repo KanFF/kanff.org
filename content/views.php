69f5213b0d17792e0b2ebe33f79e216ef60f834a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-04-26 21:19:53 +0200

    views: responsive design for text size

--- content/views.php
+++ content/views.php
@@ -6,7 +6,7 @@ function getBanner($kanff)
     ob_start();
 ?>
     <!-- <div><img src="imgs/banner-test.png" class="w-full rounded-none" alt=""></div> -->
-    <div class="w-auto py-4 xl:py-2 mb-12 mt-12 xl:flex xs:block shadow-xl overflow-hidden rounded-sm border-solid border border-gray-300">
+    <div class="w-auto py-4 xl:py-2 md:my-12 my-6 xl:flex xs:block shadow-xl overflow-hidden rounded-sm border-solid border border-gray-300">
         <div class="flex-1 flex items-center pl-2 ">
             <div class="w-full">
                 <img src="imgs/logo.svg" alt="" class="w-full h-24 border-none mb-3">
@@ -42,7 +42,7 @@ function getBanner($kanff)
 //Get HTML advantages blocks
 function getAdvantages($advantages)
 {
-    $string = "<div class='mb-12'>";
+    $string = "<div class='md:mb-12 mb-6'>";
     $textAtLeft = true; //text at left for the start element 
     foreach ($advantages as $key => $advantage) {
         $string .= buildAnAdvantage($advantage, $textAtLeft);
@@ -59,7 +59,7 @@ function buildAnAdvantage($advantage, $textAtLeft)
 {
     ob_start();
 ?>
-    <div style="background-color: #f0f0f0;" class="overflow-hidden pt-2 shadow-xl rounded-sm mb-8 border-blue-300 border border-solid <?= ($textAtLeft == false && $advantage['mode'] == 1) ? 'lg:flex-row-reverse' : '' ?>
+    <div style="background-color: #f0f0f0;" class="overflow-hidden pt-2 shadow-xl rounded-sm md:mb-8 mb-4 border-blue-300 border border-solid <?= ($textAtLeft == false && $advantage['mode'] == 1) ? 'lg:flex-row-reverse' : '' ?>
     <?php switch ($advantage['mode']) {
         case 2:
             echo 'block';
@@ -82,7 +82,7 @@ function buildAnAdvantage($advantage, $textAtLeft)
                                 echo 'm-auto';
                         } ?>">
                 <d2 class="text-2xl mb-2"><?= $advantage['name'] ?></d2>
-                <div class="text-base"><?= MDToHTML($advantage['description']) ?></div>
+                <div class="md:text-base text-sm"><?= MDToHTML($advantage['description']) ?></div>
             </div>
         </div>
         <div class="lg:mx-3 md:mx-2 mx-1 mt-2" style="flex: 4;">
@@ -99,11 +99,11 @@ function getAboutSection($about, $contributors)
 {
     ob_start();
 ?>
-    <div class=" bg-gray-100 border-blue-300 border rounded-sm shadow-xl lg:px-6 md:px-4 sm:px-3 px-2 lg:py-4 md:py-3 sm:py-2 py-1 flex items-center mb-6">
+    <div class=" bg-gray-100 border-blue-300 border rounded-sm shadow-xl lg:px-6 md:px-4 sm:px-3 px-2 lg:py-4 md:py-3 sm:py-2 py-1 flex items-center md:mb-6 mb-3">
         <div class="">
             <div>
                 <h2 class="text-2xl"><?= SECTIONS['about'] ?></h2>
-                <div class="flex-1 mr-6 mt-2">
+                <div class="flex-1 mr-6 mt-2  md:text-base text-sm">
                     <?= MDToHTML($about['intro']) ?>
                 </div>
             </div>
@@ -132,7 +132,7 @@ function getAboutSection($about, $contributors)
                                 ?>
                             </div>
                         </div>
-                        <div class="mt-2 lg:mt-0"><?= MDToHTML($about['text']) ?></div>
+                        <div class="mt-2 lg:mt-0 md:text-base text-sm"><?= MDToHTML($about['text']) ?></div>
                     </div>
                 </div>
 
@@ -166,10 +166,10 @@ function getNewsLetterSection($news)
 {
     ob_start();
 ?>
-    <div class="bg-gray-100 border-blue-300 border rounded-sm shadow-xl lg:px-6 md:px-4 sm:px-3 px-2 lg:py-4 md:py-3 sm:py-2 py-1 lg:flex block items-center mb-6">
+    <div class="bg-gray-100 border-blue-300 border rounded-sm shadow-xl lg:px-6 md:px-4 sm:px-3 px-2 lg:py-4 md:py-3 sm:py-2 py-1 lg:flex block items-center md:mb-6 mb-3">
         <div style="flex: 4">
             <h2 class="text-2xl"><?= SECTIONS['newsletter'] ?></h2>
-            <div>
+            <div class=" md:text-base text-sm">
                 <?= MDToHTML($news['text']) ?>
             </div>
         </div>
@@ -179,15 +179,15 @@ function getNewsLetterSection($news)
                     <div class="h-10 m-1 "><a target="_blank" href="<?= $link['link'] ?>"><img src="imgs/<?= $link['icon'] ?>" class="h-10" alt=""></a></div>
                 <?php } ?>
             </div>
-            <span class="text-sm text-gray-400">Lettre d'information:</span>
+            <span class="text-sm text-gray-400">Inscription à la lettre d'information</span>
             <div>
                 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
                 <script type="text/javascript" src="https://s3.amazonaws.com/phplist/phplist-subscribe-0.2.min.js"></script>
 
                 <div id="phplistsubscriberesult" class="text-green-600"></div>
                 <form action="https://kanffnews.hosted.phplist.com/lists/?p=subscribe&id=1" method="post" id="phplistsubscribeform">
-                    <input type="text" name="email" value="" id="emailaddress" class="w-full p-1 outline-none focus:bg-gray-200 rounded-sm text-black" placeholder="email@example.com" /><br>
-                    <button type="submit" id="phplistsubscribe" class="bg-gray-300 hover:bg-blue-300 px-3 py-1 rounded-sm mt-4">S'inscrire</button>
+                    <input type="text" name="email" value="" id="emailaddress" class="w-full p-1 outline-none focus:bg-gray-200 rounded-sm text-black md:text-base text-sm" placeholder="email@example.com" /><br>
+                    <button type="submit" id="phplistsubscribe" class="bg-gray-300 hover:bg-blue-300 px-3 py-1 rounded-sm mt-2">S'inscrire</button>
                     <script type="text/javascript">
                         var pleaseEnter = "";
                     </script>
