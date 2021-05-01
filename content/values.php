01a621076f080f465123d9fbd44a263cb3390232
samuelroland <47849646+samuelroland@users.noreply.github.com>
2021-05-01 14:52:46 +0200

    content: replace spaces by tabs in views.php and values.php

--- content/values.php
+++ content/values.php
@@ -11,13 +11,13 @@ $footer = $data['footer'];
 
 // Contributors are listed here and not in texts-*.json because they don't need any translations.
 $contributors = [
-    ['name' => 'Samuel Roland', 'username' => 'samuelroland', 'img' => 'https://avatars.githubusercontent.com/u/47849646?v=4', 'major' => true],
-    ['name' => 'Benoît Pierrehumbert', 'username' => 'cpnvbenoit', 'img' => 'https://avatars.githubusercontent.com/u/47849605?v=4', 'major' => true],
-    ['name' => 'LPOdev', 'username' => 'LPOdev ', 'img' => 'https://avatars.githubusercontent.com/u/47849666?v=4', 'major' => false],
-    ['name' => 'Miguel Soares', 'username' => 'miguelsoaresking500', 'img' => 'https://avatars.githubusercontent.com/u/47849626?v=4', 'major' => false],
-    ['name' => 'Simon Cuany', 'username' => 'SimonCuany', 'img' => 'https://avatars.githubusercontent.com/u/47849679?v=4', 'major' => false],
-    ['name' => 'KevinVaucher', 'username' => 'KevinVaucher', 'img' => 'https://avatars.githubusercontent.com/u/47849513?v=4', 'major' => false],
-    ['name' => 'XCarrel', 'username' => 'XCarrel', 'img' => 'https://avatars.githubusercontent.com/u/7465241?v=4', 'major' => false],
+	['name' => 'Samuel Roland', 'username' => 'samuelroland', 'img' => 'https://avatars.githubusercontent.com/u/47849646?v=4', 'major' => true],
+	['name' => 'Benoît Pierrehumbert', 'username' => 'cpnvbenoit', 'img' => 'https://avatars.githubusercontent.com/u/47849605?v=4', 'major' => true],
+	['name' => 'LPOdev', 'username' => 'LPOdev ', 'img' => 'https://avatars.githubusercontent.com/u/47849666?v=4', 'major' => false],
+	['name' => 'Miguel Soares', 'username' => 'miguelsoaresking500', 'img' => 'https://avatars.githubusercontent.com/u/47849626?v=4', 'major' => false],
+	['name' => 'Simon Cuany', 'username' => 'SimonCuany', 'img' => 'https://avatars.githubusercontent.com/u/47849679?v=4', 'major' => false],
+	['name' => 'KevinVaucher', 'username' => 'KevinVaucher', 'img' => 'https://avatars.githubusercontent.com/u/47849513?v=4', 'major' => false],
+	['name' => 'XCarrel', 'username' => 'XCarrel', 'img' => 'https://avatars.githubusercontent.com/u/7465241?v=4', 'major' => false],
 ];
 
 
