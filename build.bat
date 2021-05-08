b51a8ac77a4eed85e073ed4ed0f03ec2f30fd120
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-05-08 16:02:08 +0200

    build: update build.bat to include js folder

--- build.bat
+++ build.bat
@@ -3,7 +3,9 @@ cd dist
 cd ..
 curl localhost:8088 -o dist/index.html
 mkdir dist\imgs
+mkdir dist\js
 copy imgs dist\imgs
+copy js dist\js
 copy favicon.ico dist\favicon.ico
 copy robots.txt dist\robots.txt
 tree /F dist
